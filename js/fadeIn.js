836108cc6a0172ee8c50d6d10aba4f4720a109fa
kdh990315 <kdh990315@naver.com>
2022-05-03 19:00:06 +0900

    05.03

---
+++ js/fadeIn.js
@@ -0,0 +1,44 @@
+$(function () {
+
+        $('.slide1').click(function () {
+            $('#schedul_total_box1').fadeIn();
+            $('#schedul_total_box2').hide();
+            $('#schedul_total_box3').hide();
+            $('#schedul_total_box4').hide();
+            $('#schedul_total_box5').hide();
+        });
+
+        $('.slide2').click(function () {
+            $('#schedul_total_box1').hide();
+            $('#schedul_total_box2').fadeIn();
+            $('#schedul_total_box3').hide();
+            $('#schedul_total_box4').hide();
+            $('#schedul_total_box5').hide();
+        });
+
+        $('.slide3').click(function () {
+            $('#schedul_total_box1').hide();
+            $('#schedul_total_box2').hide();
+            $('#schedul_total_box3').fadeIn();
+            $('#schedul_total_box4').hide();
+            $('#schedul_total_box5').hide();
+        });
+
+        $('.slide4').click(function () {
+            $('#schedul_total_box1').hide();
+            $('#schedul_total_box2').hide();
+            $('#schedul_total_box3').hide();
+            $('#schedul_total_box4').fadeIn();
+            $('#schedul_total_box5').hide();
+        });
+
+        $('.slide5').click(function () {
+            $('#schedul_total_box1').hide();
+            $('#schedul_total_box2').hide();
+            $('#schedul_total_box3').hide();
+            $('#schedul_total_box4').hide();
+            $('#schedul_total_box5').fadeIn();
+        });
+
+});
+
